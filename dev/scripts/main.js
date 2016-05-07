a8ed6bd4234f4c6b2cdd045ce48c06f48f25ab51
Saman Tehrani <samanrtehrani@gmail.com>
2016-05-07 00:06:33 -0400

    sticky nav finished

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,5 +1,6 @@
 /*jslint browser: true, plusplus: true */
-/*eslint-disable no-console */
+/*eslint-disable no-console  */
+/*eslint-disable max-len  */
 /*global window, document */
 
 
@@ -14,37 +15,69 @@ var useGrid = false;
 /** END
  * GLOBAL VARIABLES
  */ 
-
-
 window.onload = function () {
     'use strict';
     
     if( useGrid ){
         gridHandler();
     }
-
-    //body = document.querySelector('body');
-    
+    var facultyNavigation = document.querySelector('#sticky-navigation');
+    var stickyNavigation = document.querySelector('#sticky-navigation .navigation');
+    var navOffsetTop = facultyNavigation.offsetTop;
+    var menuHeight = 86.6 + 5;
 
     var menuButton = document.getElementsByClassName('menu-controls')[0];
     menuButton.addEventListener('click', function(event){
         toggleMenu(event);}, false);
+    
+    if( facultyNavigation ){
+        var last_known_scroll_position = 0;
+        var ticking = false;
+
+        window.addEventListener('scroll', function() {
+            last_known_scroll_position = window.scrollY;
+            if (!ticking) {
+                window.requestAnimationFrame(function() {
+                    checkStickyNav(last_known_scroll_position);
+                    ticking = false;
+                });
+            }
+            ticking = true;
+        });
+    }
+    function checkStickyNav(scroll_pos){
+        if( (scroll_pos + menuHeight ) >= navOffsetTop ){
+            if( !stickyNavigation.classList.contains('sticked') ){
+                stickyNavigation.classList.add('sticked');   
+            }
+        }else{
+            if( stickyNavigation.classList.contains('sticked') ){
+                stickyNavigation.classList.remove('sticked');   
+            }
+        }
+        
+    }
 };
+
+
 
 
 /** START
  * MENU SECTION
  */
 function toggleMenu(){
-    console.log('menu button clicked');
     var menuPanel = document.getElementsByClassName('menu-panel')[0];
     var menuContent = menuPanel.getElementsByClassName('menu-content')[0];
     var menuControls = menuPanel.getElementsByClassName('menu-controls')[0];
+
+    var body = document.body;
     if( menuPanel.classList.contains('open') ){
+        body.classList.remove('open');
         menuPanel.classList.remove('open');
         menuContent.classList.remove('open');
         menuControls.classList.remove('open');
     }else{
+        body.classList.add('open');
         menuPanel.classList.add('open');
         menuContent.classList.add('open');
         menuControls.classList.add('open');
@@ -72,7 +105,8 @@ function gridHandler(){
     }
 
     document.onkeydown = function (evnt) {
-        if (27 === evnt.keyCode) {
+        if (27 === evnt.keyCode){
+            
             switch (gridChoice) {
             case 0:
                 gridLayer.classList.add('column-grid');
